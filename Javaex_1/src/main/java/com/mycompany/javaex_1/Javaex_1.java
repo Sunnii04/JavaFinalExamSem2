529e0b41730e26bca7a25790b294df0e1245ff5c
Sunnii04 <126378920+Sunnii04@users.noreply.github.com>
2024-07-17 19:31:03 +0700

    Add files via upload

---
+++ Javaex_1/src/main/java/com/mycompany/javaex_1/Javaex_1.java
@@ -0,0 +1,13 @@
+package com.mycompany.javaex_1;
+
+/**
+ *
+ * @author hsc
+ */
+public class Javaex_1 {
+
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        cal.setVisible(true);
+    }
+}
